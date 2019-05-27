663230781a02962db3646caf4bca161dba64338b
huming <ming1.hu@meicloud.com>
2019-05-27 09:23:24 +0800

    init

---
+++ readme.md
@@ -0,0 +1,42 @@
+###Vue-dict plugin
+
+| option   |     type       |  default |
+|:----------:|:-------------:|------:|
+| request |  `(dictKeys) => Promise` | / |
+
+####Use Case
+
+```js
+import Vue from 'vue'
+import dictPlugin from './path/to/dict'
+
+Vue.use(dictPlugin, {
+  request: (keys) => {
+    return new Promise(resolve => {
+      setTimeout(function(dict) {
+        // cacheBySessionStorage(dict)
+        resolve(dict)
+      }, 1000, [
+        // you should transform dict data to this structure
+        {
+                  dict: 'GENDER',
+                  value: [
+                    {label: '男', value: '1'},
+                    {label: '女', value: '2'}
+                  ]
+                },
+                {
+                  dict: 'TYPE',
+                  value: [
+                    {label: '高级', value: '1'},
+                    {label: '中级', value: '2'}
+                  ]
+                }
+      ])
+    })
+  }
+})
+```
+
+
+
---
+++ src/index.js
@@ -0,0 +1,79 @@
+export default {
+  install (Vue, options={}) {
+    Vue.mixin({
+      data() {
+        if (process.env.NODE_ENV !== 'production') {
+          return {
+            dict: {
+              __isDictPlugin__: true
+            }
+          }
+        }
+        return {
+          dict: {}
+        }
+      },
+      created() {
+        if (
+          process.env.NODE_ENV !== 'production'
+          && ((this.data && !this.data.__isDictPlugin__) || [0, '', null].includes(this.dict))
+        ) {
+          throw new Error(`Do not set 'dict' in data function since you use vue-dict plugin`)
+        }
+
+        const {request} = options
+        const dict = this.$options.dict
+        if (dict && Array.isArray(dict)) {
+          const dictKeys = trans2DictKeys(dict)
+          const vmDataKeys = trans2DataKeys(dict)
+          initState(this, vmDataKeys)
+          request(dictKeys).then(res => {
+            setData(this, res, dictKeys, vmDataKeys)
+          })
+        }
+      }
+    })
+  }
+}
+
+
+function trans2DictKeys(dict) {
+  return dict.reduce((acc, cur) => {
+    if (typeof cur === 'string' && cur) {
+      acc.push(cur)
+      return acc
+    }
+    if (Array.isArray(cur)) {
+      acc.push(cur[0])
+      return acc
+    }
+  }, [])
+}
+
+function trans2DataKeys(dict) {
+  return dict.reduce((acc, cur) => {
+    if (typeof cur === 'string' && cur) {
+      acc.push(cur)
+      return acc
+    }
+    if (Array.isArray(cur)) {
+      acc.push(cur.pop())
+      return acc
+    }
+  }, [])
+}
+
+function initState(vm, keys) {
+  for(let key of keys) {
+    vm.$set(vm.dict, key, [])
+  }
+}
+
+function setData(vm:, dict, dictKeys, dataKeys) {
+  dataKeys.forEach((key, idx) => {
+    let value = dict.find(_ => {
+      return  _.dict === dictKeys[idx]
+    })
+    vm.$set(vm.dict, key, (value && value.value) || [])
+  })
+}
